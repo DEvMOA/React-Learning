123316176b03eb817dbbf1e4b7d211f9680c605b
DEvMOA <moabdourahmane42@gmail.com>
2024-11-04 16:57:41 +0100

    add my project to github

---
+++ src/App.jsx
@@ -0,0 +1,21 @@
+import { useState } from 'react'
+
+function App() {
+
+  return (
+    <div className='flex items-center h-screen justify-center '>
+      <div className='grid gap-4 grid-cols-3 grid-rows-3'>
+        <div className='
+          bg-purple-700 
+            h-[150px] w-[150px] 
+            flex justify-center items-center
+            '
+        >
+          <span>1</span>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default App
